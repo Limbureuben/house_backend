e33e4957cdedbbc6e2e4236f860be17ffb3c6496
Limbureuben <limbureuben4@gmail.com>
2025-04-11 21:03:04 +0300

    changes

--- house/myapprest/admin.py
+++ house/myapprest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class HouseAdmin(admin.ModelAdmin):
+    list_display = ('house_type', 'location', 'availability_date', 'contact')
+admin.site.register(House, HouseAdmin)
