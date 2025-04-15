6293792c58f849f75631e5a3ae3f897580074933
Limbureuben <limbureuben4@gmail.com>
2025-04-15 20:44:23 +0300

    changes

--- house/myapprest/views.py
+++ house/myapprest/views.py
@@ -1,13 +1,16 @@
 from rest_framework import viewsets
 from .models import House
 from .serializers import HouseSerializer
-from rest_framework import permissions
-
+from django.contrib.auth.models import User
 
 class HouseViewSet(viewsets.ModelViewSet):
     queryset = House.objects.all()
     serializer_class = HouseSerializer
-    permission_classes = [permissions.AllowAny] 
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        user_id = self.request.data.get('user')
+        if user_id:
+            user = User.objects.get(id=user_id)  # Fetch user based on provided ID
+            serializer.save(user=user)
+        else:
+            serializer.save()
