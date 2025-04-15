40e24050fb1cf663287755190381d2209d22f640
Limbureuben <limbureuben4@gmail.com>
2025-04-15 20:42:51 +0300

    changes

--- house/myapprest/serializers.py
+++ house/myapprest/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import House
+from django.contrib.auth.models import User
 
 class HouseSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
     class Meta:
         model = House
         fields = '__all__'
