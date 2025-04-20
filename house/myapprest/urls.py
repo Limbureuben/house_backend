477d75c0b4d18645be35c3b2ce40e00104f94757
Limbureuben <limbureuben4@gmail.com>
2025-04-20 22:28:48 +0300

    changes

--- house/myapprest/urls.py
+++ house/myapprest/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path('houses/', house_list, name='house-list'),
     path('houses/<int:pk>/', house_detail, name='house-detail'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
-    path('api/book/', CreateBookingView.as_view(), name='create-booking'),
+    path('book/', CreateBookingView.as_view(), name='create-booking'),
 ]
