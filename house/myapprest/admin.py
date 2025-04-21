3b3cb4060236c15fb644b5e196bfbf48c8a7c33d
Limbureuben <limbureuben4@gmail.com>
2025-04-21 23:14:13 +0300

    changes

--- house/myapprest/admin.py
+++ house/myapprest/admin.py
@@ -10,3 +10,7 @@ admin.site.register(House, HouseAdmin)
 class BookingAdmin(admin.ModelAdmin):
     list_display = ('user', 'house', 'date_booked')
 admin.site.register(Booking, BookingAdmin)
+
+class SignedAgreementUploadAdmin(admin.ModelAdmin):
+    list_display = ('username', 'phone_number', 'uploaded_at')
+admin.site.register(SignedAgreementUpload, SignedAgreementUploadAdmin)
